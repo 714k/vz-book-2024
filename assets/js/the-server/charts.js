b8138471cd6cd135f178137d7d4cb5759decb621
714k <victor.zamudio.dev@gmail.com>
2025-06-10 04:30:24 -0600

    [VZB-117A] Create Dashboard charts - Part 1

---
+++ assets/js/the-server/charts.js
@@ -0,0 +1,235 @@
+import 'https://cdn.jsdelivr.net/npm/echarts@5.6.0/dist/echarts.min.js'
+
+const chartDonut = echarts.init(document.getElementById('chartDonut'))
+
+var optionDonut = {
+  tooltip: {
+    trigger: 'item',
+  },
+  legend: {
+    show: false,
+    top: '10%',
+    left: 'center',
+  },
+  series: [
+    {
+      name: 'Courses',
+      type: 'pie',
+      radius: ['30%', '100%'],
+      avoidLabelOverlap: false,
+      label: {
+        show: false,
+        position: 'center',
+      },
+      emphasis: {
+        label: {
+          show: false,
+          fontSize: 40,
+          fontWeight: 'bold',
+        },
+      },
+      labelLine: {
+        show: true,
+      },
+      data: [
+        {
+          value: 10,
+          name: 'In progress',
+          label: {
+            color: '#fff',
+          },
+        },
+        {
+          value: 54,
+          name: 'Completed',
+          label: {
+            name: 'Completed',
+            color: '#fff',
+          },
+          labelLine: {
+            show: false,
+          },
+        },
+      ],
+
+      color: ['#33488480', '#3d205680'],
+    },
+  ],
+}
+
+// Display the chart using the configuration items and data just specified.
+chartDonut.setOption(optionDonut)
+// ---------------------------------------------------------
+
+const chartHorizontalBar = echarts.init(
+  document.getElementById('chartHorizontalBar')
+)
+
+// Specify the configuration items and data for the chart
+const optionsHorizontalBar = {
+  title: {
+    text: 'Courses By Year',
+    itemStyle: {
+      color: 'white',
+    },
+    textStyle: {
+      fontSize: 30,
+      fontWeight: 'bold',
+      color: 'white',
+    },
+    left: 'center',
+  },
+  tooltip: {
+    trigger: 'axis',
+    axisPointer: {
+      type: 'shadow',
+    },
+  },
+  legend: {},
+  grid: {
+    left: '3%',
+    right: '4%',
+    bottom: '3%',
+    containLabel: true,
+  },
+  xAxis: {
+    type: 'value',
+    boundaryGap: [0, 0.01],
+  },
+  yAxis: {
+    type: 'category',
+    data: [
+      '2009',
+      '2010',
+      '2011',
+      '2012',
+      '2013',
+      '2014',
+      '2015',
+      '2016',
+      '2017',
+      '2018',
+      '2019',
+      '2020',
+      '2021',
+      '2022',
+      '2023',
+      '2024',
+      '2025',
+    ],
+  },
+  series: [
+    {
+      //   name: 'Courses by Year',
+      type: 'bar',
+      data: [2, 5, 3, 8, 7, 6, 12, 9, 11, 10, 15, 14, 13, 16, 18, 20, 22],
+      itemStyle: {
+        color: '#adfe00',
+      },
+      emphasis: {
+        itemStyle: {
+          color: '#adfe0080',
+        },
+      },
+      link: {
+        symbol: 'arrow',
+        symbolSize: 10,
+        color: '#adfe00',
+      },
+    },
+  ],
+}
+
+// Display the chart using the configuration items and data just specified.
+chartHorizontalBar.setOption(optionsHorizontalBar)
+
+// ---------------------------------------------------------
+
+const chartByAcademy = echarts.init(document.getElementById('chartByAcademy'))
+
+// Specify the configuration items and data for the chart
+const optionsByAcademy = {
+  title: {
+    text: 'Courses By Academy',
+    itemStyle: {
+      color: 'white',
+    },
+    textStyle: {
+      fontSize: 30,
+      fontWeight: 'bold',
+      color: 'white',
+    },
+    left: 'center',
+  },
+  tooltip: {
+    trigger: 'axis',
+    axisPointer: {
+      type: 'shadow',
+    },
+  },
+  legend: {},
+  axisLine: {
+    show: false,
+  },
+  grid: {
+    left: '3%',
+    right: '4%',
+    bottom: '3%',
+    containLabel: true,
+  },
+  xAxis: {
+    type: 'category',
+    axisLabel: {
+      rotate: 90,
+      fontSize: 14,
+      color: 'white',
+    },
+    lineStyle: {
+      opacity: 0,
+    },
+
+    data: [
+      'LinkedIn Learning',
+      'Coursera',
+      'Udacity',
+      'Codecademy',
+      'Udemy',
+      'Globant University',
+      'TutsPlus',
+      'Microsoft Learn',
+      'UNSW',
+      'CTIC',
+    ],
+  },
+  yAxis: {
+    type: 'value',
+    boundaryGap: [0, 0.01],
+  },
+  series: [
+    {
+      //   name: 'Courses by Year',
+      type: 'bar',
+      showBackground: true,
+      backgroundStyle: {
+        color: 'rgba(180, 180, 180, 0.2)',
+      },
+      data: [8, 12, 7, 3, 5, 6, 4, 10, 9, 2, 11, 1, 13],
+      itemStyle: {
+        color: '#adfe00',
+      },
+      emphasis: {
+        itemStyle: {
+          color: '#adfe0080',
+        },
+      },
+      link: {
+        symbol: 'arrow',
+        symbolSize: 10,
+        color: '#adfe00',
+      },
+    },
+  ],
+}
+
+// Display the chart using the configuration items and data just specified.
+chartByAcademy.setOption(optionsByAcademy)
